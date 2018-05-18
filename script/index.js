1938c6ae8a4963bcd54fd6304389e8a60857a07a
Brian Wang <boan.wang@gmail.com>
2018-05-18 16:22:18 -0400

    Added blinking text and fixed part of introduction description

--- script/index.js
+++ script/index.js
@@ -122,6 +122,71 @@ function projectAccordionListener() {
 	});
 }
 
+/*
+//Function for Blinking greeting (Legacy, copied from my old website)
+Function overview:
+There are 4 steps: adding letters, pausing the phrase, removing letters, and changing the phrase
+These are named accordingly to their functions.  They also determine when the $(.blinking-box) is blinking
+First, the letters are added using a setInterval (var add_phrase).  If the last letter is reached,
+	remove the setInterval, and use a setTimeout as a delay for the next function, pause_phrase
+After pausing for some time, pause_phrase runs and uses setInterval to call remove_letters continuously.
+When all letters are removed, the next function, change_phrase, is called using another setTimeout.
+This changes the phrase to the next one, or circles round and comes back to the first phrase.
+Then add_letters is called again using a setInterval, and we have a loop.
+*/	
+	
+var greeting_phrases = ["hope you enjoy your visit.","make websites for fun.", "like watching Game of Thrones.", "want to take up surfing.",
+ "can play piano kinda well."];
+var phrase_index = 0;  //Current index in the phrase list
+var current_phrase = greeting_phrases[phrase_index]; //Current phrase in the index
+var letter_index = 0; //Current index of a letter in the current phrase
+var new_greeting = "";
+var remove_phrase;  //setInterval variable for removing letters
+var add_phrase = setInterval(add_letters, 80);
+
+function add_letters() {
+	if(letter_index >= current_phrase.length) { //If the last letter is in place, end the setInterval
+		clearInterval(add_phrase);
+		$(".blinking-box").addClass("blinking"); //Makes the div "blink"
+		setTimeout(pause_phrase, 5000);  //Pauses to allow people to read the phrase
+	}
+	else {
+		$(".changing-greeting").text(new_greeting + current_phrase[letter_index]);
+		new_greeting = new_greeting + current_phrase[letter_index];
+		letter_index++;
+	}
+	
+}
+function pause_phrase() {  //Moves onto the next function, remove_phrase
+	remove_phrase = setInterval(remove_letters, 50);
+	$(".blinking-box").removeClass("blinking");
+	
+}
+function remove_letters() { //This is called by the setInterval for remove_phrase to remove all letters.
+	if(letter_index < 0) {  //Once all letters are removed, the current setInterval is removed, and there is a timeout for the phrase to change.
+		clearInterval(remove_phrase);
+		$(".blinking-box").addClass("blinking");
+		setTimeout(change_phrase,1000);
+	}
+	else {
+		new_greeting = new_greeting.substring(0,letter_index);
+		$(".changing-greeting").text(new_greeting);
+		letter_index--;
+	}
+}
+function change_phrase() {  //Changes the current phrase and restarts the relevant variables of the current phrase. Loops the cycle again
+	phrase_index++;  //Changes the phrase index in the list of phrases.
+	if(phrase_index >= greeting_phrases.length) { //Circles round if the last phrase in the list was displayed
+		phrase_index = 0;
+	}
+	current_phrase = greeting_phrases[phrase_index]; //resets current phrase
+	letter_index = 0; //resets the index of the current letter
+	new_greeting = ""; //resets the greeting
+	add_phrase = setInterval(add_letters, 80);
+	$(".blinking-box").removeClass("blinking");
+}
+//END BLINKING GREETING Functions
+
 
 $(document).ready(function(){
 	
